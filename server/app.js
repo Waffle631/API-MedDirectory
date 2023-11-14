cdbdb49587791999ff98b2f4c85de3b881ead079
Waffle <adolfo.espinosa@sansano.usm.cl>
2023-11-14 20:28:35 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,14 +4,6 @@ import cors from "cors";
 
 const app = express();
 
-const corsOptions = {
-  origin: "http://localhost:5173",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
 app.use(express.json());
 
 app.use("/doctorAPI", doctorRoutes);
